1398bccc7a03469bb21c43ea43bb471790987d34
skyepaper <111369963+skyepaper@users.noreply.github.com>
2024-12-25 17:03:01 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
-
+  const [steps, setSteps] = useState(0);
 
 function switchDisplay(word) {
   let arr=document.querySelectorAll('.display')
@@ -12,6 +12,107 @@ function switchDisplay(word) {
 
   let find=document.querySelector(`.${word}`)
   if(find)find.style.display='block'
+}
+
+function startCount() {
+
+  let boxA=document.querySelector('.box-first')
+  let boxB=document.querySelector('.box-second')
+
+  let flag=1
+  let count=0
+  let isMobile=0
+
+window.addEventListener('devicemotion', function(event) {
+  
+    let accX = event.accelerationIncludingGravity.x;
+    let accY = event.accelerationIncludingGravity.y;
+    let accZ = event.accelerationIncludingGravity.z;
+
+    if(accX) {
+      isMobile=1
+    }
+
+    if(isMobile===1) {
+
+      let curX=Math.ceil(Math.abs(accX.toFixed(2))*100)   
+      let curY=Math.ceil(Math.abs(accY.toFixed(2))*100)   
+      let curZ=Math.ceil(Math.abs(accZ.toFixed(2))*100)  
+      let sum=curX+curY+curZ
+      if(sum<1000)sum=1000
+      
+      if(flag===1) {
+        flag=0
+        
+         let divX=document.createElement('div')
+         divX.classList.add('mark')
+         divX.textContent=sum
+         boxA.appendChild(divX)
+        
+         setTimeout(()=>{flag=1},50)
+      }
+    }
+      
+})
+
+if(isMobile===1) {
+
+  let interval=setInterval(()=>{
+  
+  
+    boxB.innerHTML=boxA.innerHTML
+    boxA.innerHTML=''
+  
+   let arr=boxB.querySelectorAll('.mark')
+   
+   if(arr.length>10) {
+      let min=arr[0].textContent
+      let max=arr[0].textContent
+      let num=0
+      
+      arr.forEach(mark=>{
+         num=mark.textContent
+        
+         if(num<min) min=num
+         if(num>max) max=num
+      })
+       if(max<min) {
+           arr.forEach(mark=>{
+             num=mark.textContent
+             if(num<min) min=num
+             if(num>max) max=num
+           })
+       }
+     
+     let diff=max-min
+     if(diff>500) {
+         let percent=0
+         arr.forEach(mark=>{
+           num=mark.textContent
+           percent=((num-min)/(max-min))*100
+           mark.style.height=`${percent}%`
+           if(percent>80) {
+             mark.style.backgroundColor='blue'
+             count++
+             setSteps(count)
+           } else if(percent>60) {
+              mark.style.backgroundColor='lightblue'
+           } else if(percent>40) {
+              mark.style.backgroundColor='lightblue'
+           } else if(percent>20) {
+              mark.style.backgroundColor='orange'
+           } else  {
+              mark.style.backgroundColor='black'
+           }
+         })
+     }
+      
+   }
+   
+  },4000)
+
+}
+
 }
 
 
@@ -38,16 +139,18 @@ function switchDisplay(word) {
    </div>
    
    <div className="display count" >
-      <div className="steps">1111</div>
+      <div className="steps">1000</div>
+      <div class="box-first"></div>
+      <div class="box-second"></div>
    </div>
 
    <div className="display tree" >
-      <div className="steps">2222</div>
+      <div className="steps">1000</div>
    </div>
 
    <div className="display map" >map</div>
 
-   <button type="button" className="start">
+   <button type="button" className="start" onClick={startCount}>
       <div class="top">START</div>
       <div class="bottom"></div>
    </button>
